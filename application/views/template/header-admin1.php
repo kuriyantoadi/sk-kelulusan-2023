0e4a4cdf7e52013db65b4ef8aab13182d817884e
kuriyantoadi <melatikuning212@gmail.com>
2023-04-21 13:27:04 +0700

    update

---
+++ application/views/template/header-admin1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Tampil dan Download Surat Keputusan Kelulusan </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/login/css/bootstrap.min.css">
+
+  <script src="<?= base_url() ?>assets/login/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="<?= base_url() ?>assets/login/js/jquery-latest.js"></script>
+</head>
+
+<body>
